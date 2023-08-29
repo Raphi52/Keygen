549b53ca20f134d10d040fc7a774cfd6f2e6efbb
Raphi52 <raphi52@hotmail.fr>
2023-08-29 08:55:43 +0200

    Add files via upload

---
+++ LicenseGenerator/LicenseGenerator/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace LicenseGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonGenerate = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.textBoxMac = new System.Windows.Forms.TextBox();
+            this.labelMAC = new System.Windows.Forms.Label();
+            this.labelDateFin = new System.Windows.Forms.Label();
+            this.textBoxResult = new System.Windows.Forms.TextBox();
+            this.textBoxProgram = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.textBoxProg = new System.Windows.Forms.TextBox();
+            this.labelProgName = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelPasValide = new System.Windows.Forms.Label();
+            this.labelValide = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxTest = new System.Windows.Forms.TextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonGenerate
+            // 
+            this.buttonGenerate.BackColor = System.Drawing.SystemColors.Highlight;
+            this.buttonGenerate.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonGenerate.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.buttonGenerate.Location = new System.Drawing.Point(297, 49);
+            this.buttonGenerate.Name = "buttonGenerate";
+            this.buttonGenerate.Size = new System.Drawing.Size(106, 40);
+            this.buttonGenerate.TabIndex = 0;
+            this.buttonGenerate.Text = "Générer";
+            this.buttonGenerate.UseVisualStyleBackColor = false;
+            this.buttonGenerate.Click += new System.EventHandler(this.buttonGenerate_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(89, 58);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 1;
+            // 
+            // textBoxMac
+            // 
+            this.textBoxMac.Location = new System.Drawing.Point(57, 6);
+            this.textBoxMac.Name = "textBoxMac";
+            this.textBoxMac.Size = new System.Drawing.Size(255, 20);
+            this.textBoxMac.TabIndex = 2;
+            // 
+            // labelMAC
+            // 
+            this.labelMAC.AutoSize = true;
+            this.labelMAC.Location = new System.Drawing.Point(18, 9);
+            this.labelMAC.Name = "labelMAC";
+            this.labelMAC.Size = new System.Drawing.Size(30, 13);
+            this.labelMAC.TabIndex = 3;
+            this.labelMAC.Text = "MAC";
+            // 
+            // labelDateFin
+            // 
+            this.labelDateFin.AutoSize = true;
+            this.labelDateFin.Location = new System.Drawing.Point(16, 64);
+            this.labelDateFin.Name = "labelDateFin";
+            this.labelDateFin.Size = new System.Drawing.Size(62, 13);
+            this.labelDateFin.TabIndex = 4;
+            this.labelDateFin.Text = "Date de Fin";
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.Location = new System.Drawing.Point(29, 99);
+            this.textBoxResult.Multiline = true;
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.ReadOnly = true;
+            this.textBoxResult.Size = new System.Drawing.Size(356, 20);
+            this.textBoxResult.TabIndex = 5;
+            // 
+            // textBoxProgram
+            // 
+            this.textBoxProgram.Location = new System.Drawing.Point(89, 32);
+            this.textBoxProgram.Name = "textBoxProgram";
+            this.textBoxProgram.Size = new System.Drawing.Size(163, 20);
+            this.textBoxProgram.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Programme";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(1, 2);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(415, 160);
+            this.tabControl1.TabIndex = 8;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.labelMAC);
+            this.tabPage1.Controls.Add(this.textBoxMac);
+            this.tabPage1.Controls.Add(this.dateTimePicker1);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.textBoxResult);
+            this.tabPage1.Controls.Add(this.textBoxProgram);
+            this.tabPage1.Controls.Add(this.buttonGenerate);
+            this.tabPage1.Controls.Add(this.labelDateFin);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(407, 134);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Générer";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(318, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 31);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Use My Mac";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.textBoxProg);
+            this.tabPage2.Controls.Add(this.labelProgName);
+            this.tabPage2.Controls.Add(this.button1);
+            this.tabPage2.Controls.Add(this.labelPasValide);
+            this.tabPage2.Controls.Add(this.labelValide);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.textBoxTest);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(407, 134);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Tester";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // textBoxProg
+            // 
+            this.textBoxProg.Location = new System.Drawing.Point(159, 25);
+            this.textBoxProg.Name = "textBoxProg";
+            this.textBoxProg.Size = new System.Drawing.Size(242, 20);
+            this.textBoxProg.TabIndex = 6;
+            // 
+            // labelProgName
+            // 
+            this.labelProgName.AutoSize = true;
+            this.labelProgName.Location = new System.Drawing.Point(7, 28);
+            this.labelProgName.Name = "labelProgName";
+            this.labelProgName.Size = new System.Drawing.Size(102, 13);
+            this.labelProgName.TabIndex = 5;
+            this.labelProgName.Text = "Nom Du Programme";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(162, 94);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 34);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Tester";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelPasValide
+            // 
+            this.labelPasValide.AutoSize = true;
+            this.labelPasValide.Location = new System.Drawing.Point(324, 99);
+            this.labelPasValide.Name = "labelPasValide";
+            this.labelPasValide.Size = new System.Drawing.Size(57, 13);
+            this.labelPasValide.TabIndex = 3;
+            this.labelPasValide.Text = "Pas Valide";
+            // 
+            // labelValide
+            // 
+            this.labelValide.AutoSize = true;
+            this.labelValide.Location = new System.Drawing.Point(54, 99);
+            this.labelValide.Name = "labelValide";
+            this.labelValide.Size = new System.Drawing.Size(36, 13);
+            this.labelValide.TabIndex = 2;
+            this.labelValide.Text = "Valide";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Key";
+            // 
+            // textBoxTest
+            // 
+            this.textBoxTest.Location = new System.Drawing.Point(57, 53);
+            this.textBoxTest.Name = "textBoxTest";
+            this.textBoxTest.Size = new System.Drawing.Size(344, 20);
+            this.textBoxTest.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 154);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "EDKeyGen";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonGenerate;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox textBoxMac;
+        private System.Windows.Forms.Label labelMAC;
+        private System.Windows.Forms.Label labelDateFin;
+        private System.Windows.Forms.TextBox textBoxResult;
+        private System.Windows.Forms.TextBox textBoxProgram;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label labelValide;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxTest;
+        private System.Windows.Forms.Label labelPasValide;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBoxProg;
+        private System.Windows.Forms.Label labelProgName;
+    }
+}
+
---
+++ LicenseGenerator/LicenseGenerator/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.IO;
+using System.Net.NetworkInformation;
+using System.Security.Cryptography;
+using System.Text;
+
+using System.Windows.Forms;
+
+namespace LicenseGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            labelValide.Hide();
+            labelPasValide.Hide();
+        }
+
+        byte[] key = Encoding.UTF8.GetBytes("0123456789ABCDEF"); // La clé doit être de 128, 192 ou 256 bits (16, 24 ou 32 bytes).
+        byte[] iv = Encoding.UTF8.GetBytes("1234567890ABCDEF"); // Le vecteur d'initialisation (IV) doit être de 16 bytes.
+        private void buttonGenerate_Click(object sender, EventArgs e)
+        {
+            string plainText = dateTimePicker1.Value.ToString("yyyy-MM-dd");
+
+
+            
+            if (!textBoxProgram.Text.Equals(""))
+                plainText += textBoxProgram.Text;
+            if (!textBoxMac.Text.Equals(""))
+                plainText += textBoxMac.Text;
+
+
+
+            textBoxResult.Text = EncryptString(plainText, key, iv);
+
+        }
+
+        public string EncryptString(string plainText, byte[] key, byte[] iv)
+        {
+            using (AesCryptoServiceProvider aes = new AesCryptoServiceProvider())
+            {
+                aes.Key = key;
+                aes.IV = iv;
+
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+                byte[] plaintextBytes = Encoding.UTF8.GetBytes(plainText);
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                    {
+                        cs.Write(plaintextBytes, 0, plaintextBytes.Length);
+                    }
+                    byte[] encryptedBytes = ms.ToArray();
+                    return Convert.ToBase64String(encryptedBytes);
+                }
+            }
+        }
+
+
+        public string DecryptString(string cipherText, byte[] key, byte[] iv)
+        {
+
+            using (AesCryptoServiceProvider aes = new AesCryptoServiceProvider())
+            {
+                aes.Key = key;
+                aes.IV = iv;
+
+                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+              
+                
+                using (MemoryStream ms = new MemoryStream(Convert.FromBase64String(cipherText)))
+                using (CryptoStream cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
+                using (StreamReader sr = new StreamReader(cs))
+                {
+                    return sr.ReadToEnd();
+                }
+            }
+        }
+    
+            
+        
+        public string myMACAdress()
+        {
+            string macAddress = string.Empty;
+
+            foreach (NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (nic.OperationalStatus == OperationalStatus.Up)
+                {
+                    macAddress = nic.GetPhysicalAddress().ToString();
+                    break;
+                }
+            }
+            return macAddress;
+        }
+        
+
+    public string GetLicenseText()
+    {
+        string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "license.txt");
+
+        if (File.Exists(filePath))
+        {
+               string result =  File.ReadAllText(filePath);
+                return result;
+        }
+
+        return string.Empty; // Return an empty string if the file does not exist or cannot be accessed.
+    }
+     
+
+    public void WriteLicenseText(string content)
+    {
+        string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "license.txt");
+
+        File.WriteAllText(filePath, content);
+    }
+        
+        public bool validateKey(string testedString)
+        {
+            string programName = textBoxProg.Text;
+            string decryptedString = DecryptString(testedString, key, iv);
+           
+            // Extraire la partie de la clé correspondant à la date
+            string dateString = decryptedString.Substring(0, 10);
+
+            
+
+                if (DateTime.TryParse(dateString, out DateTime date))
+                {
+                    if (date < DateTime.Now)
+                    {
+                        labelPasValide.Show();
+                    return false;
+                    }
+                }
+
+            if (decryptedString.Substring(10).Equals(programName) || decryptedString.Substring(10).Equals(programName + myMACAdress()))
+            {
+                labelValide.Show();
+                return true;
+            }
+
+            labelPasValide.Show();
+            return false;
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+
+        {
+            labelValide.Hide();
+            labelPasValide.Hide();
+            if (!textBoxTest.Equals(""))
+            WriteLicenseText(textBoxTest.Text);
+            string testedString = GetLicenseText();
+            validateKey(testedString);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBoxMac.Text = myMACAdress();
+
+        }
+    }
+}
